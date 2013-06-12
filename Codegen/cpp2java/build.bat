ef295ab927b06970c6d78f0ca8a722854a221920
caprico <caprico@localhost>
2013-06-12 20:33:30 +0200

    Cleanup cpp2java

--- Codegen/cpp2java/build.bat
+++ Codegen/cpp2java/build.bat
@@ -4,8 +4,7 @@ mkdir build\doxyxml 2>nul
 bin\doxygen.exe ogre4j.doxygen
 
 copy /y fixedDoxygen\*.xml build\doxyxml
-copy /y xslt\mycombine.xslt build\doxyxml\ >nul
 
-bin\transform.exe -s:build\doxyxml\index.xml -xsl:build\doxyxml\mycombine.xslt -o:build\all.xml
-bin\transform.exe -s:build\all.xml -xsl:xslt\input.xslt -o:build\meta.xml
+bin\transform.exe -s:build\doxyxml\index.xml -xsl:build\doxyxml\combine.xslt -o:build\all.xml
+bin\transform.exe -s:build\all.xml -xsl:input.xslt -o:build\meta.xml
 
